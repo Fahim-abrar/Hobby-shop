b47e9743fe3946882f90268faa4ade4b73fea73f
Fahim <fahimabrar191336@gmail.com>
2021-07-07 19:15:23 +0600

    customer profile change

--- src/Customer.js
+++ src/Customer.js
@@ -1,42 +1,41 @@
-import React from 'react'
-import "./customer.css"
-
+import React from "react";
+import "./customer.css";
 
 function Customer() {
-    return (
-      <div className="customer">
-        <div className="customer-name">
-          My Orders Wishlist 8 My ratings My reviews Notifications Setting
-          ------------------------------ Logout
-          <div className="customer-profile">this is profile</div>
+  return (
+    <div className="customer">
+      <div className="customer-name">
+        My Orders Wishlist 8 My ratings My reviews Notifications Setting
+        ------------------------------ Logout
+        <div className="customer-profile">this is profile</div>
+      </div>
+      <div className="customer-card">
+        <div className="customer-image">
+          <img src="" alt="" />
         </div>
-        <div className='customer-card'>
-          <div className='customer-image'>
-               <img src="" alt=''/>
+        <div className="customer_content">
+          <div className="customer-title">
+            <h3>Customer Profile</h3>
           </div>
-            <div className='customer_content'>
-                <div className='customer-title'>
-                    <h3>Customer Profile</h3>
-                </div>
-                <div className='customer-body'>
-                    <p>First Name: 
-                    Last Name: 
-                    E-mail: 
-                    Birthday: </p>
-                </div>
-            </div>
-
-          <div className='btn'>
-              <button>
-                  <a>
-                      Change Password
-                  </a>
-              </button>
+          <div className="customer-body">
+            <div className="customer-info">First Name:</div>
+            <div className="customer-info">Last Name:</div>
+            <div className="customer-info">Email:</div>
           </div>
-            
+          <div className="bton">
+            <button>
+              <a>Change Password</a>
+            </button>
+          </div>
+          <div className="customer-password">
+            <button>
+              <a>Edit profile</a>
+            </button>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
-export default Customer
+export default Customer;
